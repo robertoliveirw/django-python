9070b48080e25b6cace29b3b846903ad874c6e79
Roberto <oliveirarobertw@gmail.com>
2025-02-05 12:23:53 -0300

    org. urls2

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
+    path('admin/', admin.site.urls), 
 ]
