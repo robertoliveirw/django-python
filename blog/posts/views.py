a408cdf31540a5fa4eb1cf76deecd13dfc8edb2d
Roberto <oliveirarobertw@gmail.com>
2025-02-10 13:52:15 -0300

    teste

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def helloworld(request):
-    return HttpResponse('Hello World' )
+    return HttpResponse('Hello World<br>I´m testing this method')
+
