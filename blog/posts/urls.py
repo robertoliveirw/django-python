3eeed667d45c56b0ede6fdf615de3b1769fb4f77
Roberto <oliveirarobertw@gmail.com>
2025-02-10 17:03:30 -0300

    second view done

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.helloworld)
+    path('helloworld/', views.helloworld),
+    path('secondview/', views.secondview),
 ]
