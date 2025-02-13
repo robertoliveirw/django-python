2c232cde65bcdf7138731aca08629e51de1869ba
Roberto <oliveirarobertw@gmail.com>
2025-02-13 15:32:16 -0300

    from \ to /

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -28,9 +28,9 @@ def home(request):
     for post in posts:
         html += f'''
             <div>
-                <h1>{post['id']} - {post['title']}<\h1>
-                <p>{post['content']}<\p>
-            <\div>'''
+                <h1>{post['id']} - {post['title']}</h1>
+                <p>{post['content']}</p>
+            </div>'''
     return HttpResponse(html)
 
 def post(request, id):
